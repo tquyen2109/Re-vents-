68e43a07299e6032d2eedfef9fbeab9657123235
tquyen2109 <t.quyen1997@yahoo.com>
2021-02-14 17:56:32 -0500

    Complete CRUD UI

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,19 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Grid } from 'semantic-ui-react'
 import EventForm from '../eventForm/EventForm'
 import EventList from './EventList'
-import {sampleData} from '../../../app/api/sampleData'
-export default function EventDashboard ({formOpen, setFormOpen}) {
+import { sampleData } from '../../../app/api/sampleData'
+export default function EventDashboard({ formOpen, setFormOpen, selectEvent, selectedEvent }) {
     const [events, setEvents] = useState(sampleData);
-   
+
+
+    function handleCreateEvent(event) {
+        setEvents([...events, event]);
+    }
+    function handleUpdateEvent(updatedEvent) {
+        setEvents(events.map(event => event.id === updatedEvent.id ? updatedEvent : event));
+        selectEvent(null);
+    }
+    function handleDeleteEvent(eventId) {
+        setEvents(events.filter(event => event.id !== eventId));
+    }
     return (
         <Grid>
             <Grid.Column width={10}>
-                <EventList events={sampleData} />
+                <EventList events={events} selectEvent={selectEvent} deleteEvent={handleDeleteEvent} />
             </Grid.Column>
             <Grid.Column width={6}>
                 {/* only display if formOpen =true */}
-                {formOpen && <EventForm setFormOpen={setFormOpen}/> }
+                {formOpen && <EventForm setFormOpen={setFormOpen} setEvents={setEvents} createEvent={handleCreateEvent} selectedEvent={selectedEvent} key={selectedEvent ? selectedEvent.id : null} updateEvent={handleUpdateEvent} />}
             </Grid.Column>
         </Grid>
     )
--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Button, Icon, Item, List, Segment }from 'semantic-ui-react'
 import EventListAttendee from './EventListAttendee';
-function EventListItem({event}) {
+function EventListItem({event, selectEvent, deleteEvent}) {
     return (     
         <Segment.Group>
             <Segment>
@@ -33,7 +33,8 @@ function EventListItem({event}) {
             </Segment>
             <Segment clearing>
                 <div>{event.description}</div>
-                <Button color='teal' floated='right' content='View'/>
+                <Button onClick={() => selectEvent(event)} color='teal' floated='right' content='View'/>
+                <Button onClick={() => deleteEvent(event.id)} color='red' floated='right' content='Delete'/>
             </Segment>
         </Segment.Group>    
     )
